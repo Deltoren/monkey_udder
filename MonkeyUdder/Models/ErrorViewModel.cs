7ef778b664a3869fccf23db43a5c2cd8a89f2b8e
『 Pavel 』 <gaagpavel01@gmail.com>
2021-09-22 21:45:20 +0700

    first commit

---
+++ MonkeyUdder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonkeyUdder.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
